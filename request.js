f3c890de3448e06ddd918e47a37b4cb818230b59
Tom Matos <tosma38320@gmail.com>
2020-09-30 16:33:13 +0200

    Finalisation fonction regex + POST

--- request.js
+++ request.js
@@ -3,7 +3,7 @@
 // Produits //
 let panier = document.querySelectorAll('.add');       //Selection bouton ajoutez au panier
 let url = "http://localhost:3000/api/cameras";
-let item ={};
+let item = {};
 let params = new URLSearchParams(document.location.search.substring(1));
 document.querySelector('#compteurPanier').textContent = localStorage.getItem('cartNumbers');
 
@@ -29,8 +29,8 @@ switch (window.location.pathname) {
         console.log('Panier');
         afficherPanier();
         document.querySelector('.buy').addEventListener('click', e => {
-            let inputs = document.querySelectorAll('input.form-control')
-            ValidRegex(inputs)
+            e.preventDefault();
+            ValidReg();
         })
         break
     case '/C:/Users/tosma/Desktop/Orinoco/JWDP5/order.html':
@@ -42,10 +42,10 @@ switch (window.location.pathname) {
 
 
 for (let i = 0; i < panier.length; i++) {
-    panier[i].addEventListener('click', function(e) {                //Evenement au clic sur bouton panier
+    panier[i].addEventListener('click', function (e) {                //Evenement au clic sur bouton panier
         e.preventDefault();
         cartNum();                                                  // Appel fonction pour nbr article
-        itemPanier();                                                // Appel fonction pour ajouter un item au panier
+    itemPanier();                                                   // Appel fonction pour ajouter un item au panier
     })
 }
 
@@ -53,31 +53,31 @@ for (let i = 0; i < panier.length; i++) {
 
 //Récuperation donnée API
 function list(url) {
-    fetch (url).then (function (response) {
-            if (response.status !== 200) {
-                console.log('Problème requete. Code erreur : ' + response.statut);
-                return;
-            }
-                response.json().then(function (data) {
-                    for (let i = 0; i < data.length; i++) {       //Boucle pour parcourir les valeurs de retour
-                        let mainContentImage = document.getElementById("camera"); 
-                        let div = document.createElement("div");
-                        div.className = "col-md-4 my-4";
-                        div.innerHTML =`<div class="card shadow border-0">
+    fetch(url).then(function (response) {
+        if (response.status !== 200) {
+            console.log('Problème requete. Code erreur : ' + response.statut);
+            return;
+        }
+        response.json().then(function (data) {
+            for (let i = 0; i < data.length; i++) {       //Boucle pour parcourir les valeurs de retour
+                let mainContentImage = document.getElementById("camera");
+                let div = document.createElement("div");
+                div.className = "col-md-4 my-4";
+                div.innerHTML = `<div class="card shadow border-0">
                                             <img src="${data[i].imageUrl}" alt="img" class="my-3" height="240px">
                                                 <div class="card-body">
                                                     <h5 class="card-title">${data[i].name}</h5>
                                                     <p class="card-text">${data[i].description}</p>        
                                                     <a href="produits.html?id=${data[i]._id}" class="btn btn-primary">Détails</a>                                                                                           
                                                 </div>
-                                        </div> `;       
-                        mainContentImage.appendChild(div);
-                    }
-            });
-        })
+                                        </div> `;
+                mainContentImage.appendChild(div);
+            }
+        });
+    })
         .catch(function (error) {
             console.log('Problème avec fonction fetch : ', error);
-    });
+        });
 }
 
 
@@ -89,135 +89,119 @@ function product(id) {
 //Récupération id des produits API, pour envoyer infos sur page produits.html?id
 function produit(id) {
     fetch(url + '/' + id).then(function (response) {
-            if (response.ok) {
-                response.json().then(function (json) {
-                    document.querySelector("#name").textContent = json.name;
-                    document.querySelector("#desc").textContent = json.description;
-                    document.querySelector('img').setAttribute('src', json.imageUrl);
-                    document.querySelector('.idImage').setAttribute('src', json.imageUrl);
-                    document.querySelector('#price').textContent = json.price / 100 + ' €';
-                    document.querySelector('#price').className = "font-weight-bold my-5 ml-5";
-                    document.querySelector('.idImage').className = "img-fluid border border-secondary rounded my-5 productImage";
-                    document.querySelector('p').className = "my-5 ml-5 h3";
-                    document.querySelector('.add').setAttribute('id', json._id);
-                
-                    //Boucle pour tout les objectifs retournés en valeur, on a attribue la valeur des objectifs dans la balise option
-                    for (let i in json.lenses) {
-                        let details = document.querySelector('#lenses');
-                        let option = document.createElement("option");
-                        option.innerHTML = `<option id="${json.lenses[i]}" value="${json.lenses[i]}" name="${json.lenses[i]}">${json.lenses[i]}</option>`;
-                        details.appendChild(option);
-                    }
-                })
-            }
+        if (response.ok) {
+            response.json().then(function (json) {
+                document.querySelector("#name").textContent = json.name;
+                document.querySelector("#desc").textContent = json.description;
+                document.querySelector('img').setAttribute('src', json.imageUrl);
+                document.querySelector('.idImage').setAttribute('src', json.imageUrl);
+                document.querySelector('#price').textContent = json.price / 100 + ' €';
+                document.querySelector('#price').className = "font-weight-bold my-5 ml-5";
+                document.querySelector('.idImage').className = "img-fluid border border-secondary rounded my-5 productImage";
+                document.querySelector('p').className = "my-5 ml-5 h3";
+                document.querySelector('.add').setAttribute('id', json._id);
+
+                //Boucle pour tout les objectifs retournés en valeur, on a attribue la valeur des objectifs dans la balise option
+                for (let i in json.lenses) {
+                    let details = document.querySelector('#lenses');
+                    let option = document.createElement("option");
+                    option.innerHTML = `<option id="${json.lenses[i]}" value="${json.lenses[i]}" name="${json.lenses[i]}">${json.lenses[i]}</option>`;
+                    details.appendChild(option);
+                }
+            })
+        }
     })
 }
 
+// Fonction validation formulaire, si Ok renvoi vers page order avec les valeurs correspondantes
 
-
-function ValidRegex(inputs) {
-    let data = {}
-    let valid = 0
-    let reg = {
+function ValidReg() {
+    let inputs = document.querySelectorAll('input.form-control');
+    let data = {};
+    let valid = true;
+    let regex = {
         "mail": /([\w-\.]+@[\w-\.]+\.{1}[\w]+)/i, // Adresse Mail
         "text": /^\S[a-z ,.'à-ÿ-]+$/i, // Nom, Prénom, Ville
-        "postalcode": /^[0-9]{5}$/i, // Code postal
         "postal": /^[0-9]{1,5}[A-z0-9 'à-ÿ-]{5,30}$/i // Adresse postale
     }
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].name == "name" || inputs[i].name == "lastname" || inputs[i].name == "city") {
-            formV(reg.text.test(inputs[i].value), inputs[i])
-        } else if (inputs[i].name == "email") {
-            formV(reg.mail.test(inputs[i].value), inputs[i])
-        } else if (inputs[i].name == "adress") {
-            formV(reg.postal.test(inputs[i].value), inputs[i])
-        } else if (inputs[i].name == "postalcode") {
-            formV(reg.postalcode.test(inputs[i].value), inputs[i])
-        }
-        if (inputs[i].classList[1] == 'is-valid') {
-            valid = i
-        }
-        if (valid = 5) {
-            document.querySelector('form').submit()
-            let pducts = JSON.parse(localStorage.getItem('itemsInCart'))
-            data = {
-                'contact': {
-                    'firstName': inputs[0].value,
-                    'lastName': inputs[1].value,
-                    'address': inputs[3].value,
-                    'city': inputs[4].value,
-                    'email': inputs[2].value
-                },
-                'products': [
+    inputs.forEach(input => {
+        let validation = false;
+        if (input.name == "name" || input.name == "lastname" || input.name == "city")
+            validation = validForm(regex.text.test(input.value), input);
+        else if (input.name == "email")
+            validation = validForm(regex.mail.test(input.value), input);
+        else if (input.name == "adress")
+            validation = validForm(regex.postal.test(input.value), input);
+        if (!validation)
+            valid = false;
+    });
 
-                ]
-            }
-            Object.keys(pducts).map(pdt => {
-                for (let i = 0; i < pducts[pdt].length; i++) {
-                    const e = pducts[pdt][i];
-                    if (e.incart > 1) {
-                        for (let x = 0; x < e.incart; x++) {
-                            console.log(e)
-                            data.products.push(e.id)
-                        }
-                    } else {
-                        data.products.push(e.id)
+    if (valid) {
+        console.log('test envoi');
+        let pducts = JSON.parse(localStorage.getItem('itemsInCart'));
+        data = {
+            'contact': {
+                'firstName': inputs[0].value,
+                'lastName': inputs[1].value,
+                'address': inputs[3].value,
+                'city': inputs[4].value,
+                'email': inputs[2].value
+            },
+            'products': [
+
+            ]
+        }
+
+        // Recupération des proprietés et valeurs du produits dans le panier
+        Object.keys(pducts).map(pdt => {
+            for (let i = 0; i < pducts[pdt].length; i++) {
+                const e = pducts[pdt][i];
+                if (e.incart > 1) {
+                    for (let x = 0; x < e.incart; x++) {
+                        data.products.push(e.id);
                     }
+                } else {
+                    data.products.push(e.id);
                 }
-            })
-            console.log(data)
-            // Ici le post et localstorage
-            let posturl = "http://localhost:3000/api/cameras/order"
-            const options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
             }
-            fetch(posturl, options).then((response) => {
-
-                response.json().then((data) => {
-                    console.log(data);
-                    localStorage.setItem('order', JSON.stringify(data))
-                    localStorage.removeItem('cartNumbers')
-                    localStorage.removeItem('totalCost')
-                    localStorage.removeItem('itemsInCart')
-                    document.location = 'order.html?order=' + data.orderId
-                });
-            })
+        })
+        console.log(data)
+        // Ici le Post et localstorage
+        let posturl = "http://localhost:3000/api/cameras/order"
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
         }
+        fetch(posturl, options).then((response) => {
+
+            response.json().then((data) => {
+                console.log(data);
+                localStorage.setItem('order', JSON.stringify(data));
+                localStorage.removeItem('cartNumbers');
+                localStorage.removeItem('totalCost');
+                localStorage.removeItem('itemsInCart');
+                document.location = 'order.html?order=' + data.orderId;
+            });
+        })
     }
-
-}
-    
-
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
 
 
 // Fonction ajout ou suppression validation formulaire
-function formV(Regtest, input) {
+function validForm(Regtest, input) {
     if (Regtest) {
         input.classList.remove("is-invalid");
         input.classList.add("is-valid");
+        return true;
     } else {
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
     }
+    return false;
 }
-
-
-
-// function chargementPanierNumbers() {                                // Fonction pour garder cartNum même en rafraichissant la page
-//     let cartNumbers = localStorage.getItem('cartNumbers');
-//     if (cartNumbers) {
-//         document.querySelector('#compteurPanier').textContent = cartNumbers;
-//     } else {
-//         document.querySelector('#compteurPanier').textContent = 0;
-//     }
-// }
 
 
 
@@ -237,31 +221,31 @@ function cartNum() {
 // Fonction ajout item dans localstorage et panier en fonction de son id
 
 function itemPanier() {
-    if (localStorage.getItem('itemsInCart') != null) {                     // 1) Si item dans panier sont # de null
-
-        let inCart = JSON.parse(localStorage.getItem('itemsInCart')) ;     // 2) Récuperer les items dans un tableau
+    if (localStorage.getItem('itemsInCart') != null) {                      // 1) Si item dans panier sont # de null
+ 
+        let inCart = JSON.parse(localStorage.getItem('itemsInCart'));       // 2) Récuperer les items dans un tableau
 
         let product = updateproduct();
         total();
         console.log(product);
 
         let ids = Object.keys(inCart);                                      // Retourne un tableau des noms de proprietes de l'objet inCart
-            if (ids.indexOf(product[0].id) != -1) {                        // Si l'id est déjà dans le panier 
-                let i = ids.indexOf(product[0].id);
-                let curid = ids[i];
-                let info = 0;
-                inCart[curid][info].incart += 1;
-                localStorage.setItem('itemsInCart', JSON.stringify(inCart));        // 3) Ajouter le nouvel item au tableau product
-                // console.log([i]);  TEST                                      
-            } else {
-                localStorage.setItem('itemsInCart', JSON.stringify(addItem(product[0].id, product, inCart)));    // Ajouter au localstorage
-            }
+        if (ids.indexOf(product[0].id) != -1) {                             // Si l'id est déjà dans le panier 
+            let i = ids.indexOf(product[0].id);
+            let curid = ids[i];
+            let info = 0;
+            inCart[curid][info].incart += 1;
+            localStorage.setItem('itemsInCart', JSON.stringify(inCart));    // 3) Ajouter le nouvel item au tableau product
+            // console.log([i]);  TEST                                      
+        } else {
+            localStorage.setItem('itemsInCart', JSON.stringify(addItem(product[0].id, product, inCart)));    // Ajouter au localstorage
+        }
 
     } else {
         let product = updateproduct();
-            total();
-            localStorage.setItem('itemsInCart', JSON.stringify(addItem(product[0].id, product))) ;   // 4) Ajouter le nouveau tableau dans le localstorage
-        }
+        total();
+        localStorage.setItem('itemsInCart', JSON.stringify(addItem(product[0].id, product)));   // 4) Ajouter le nouveau tableau dans le localstorage
+    }
 }
 
 //Fonction renvoi des éléments de proprieté des produits
@@ -276,7 +260,7 @@ function updateproduct() {
     }]
 
     return product;
-    
+
 }
 
 //Fonction ajout d'un produit
@@ -305,22 +289,23 @@ function deleteItem(id) {
             const itemcount = new Promise((resolve, reject) => {
                 resolve(json[id][0].incart);
             })
-            .then((items) => {
-                let num = parseInt(localStorage.getItem('cartNumbers'));
-                num = num - items;
+                .then((items) => {
+                    let num = parseInt(localStorage.getItem('cartNumbers'));
+                    num = num - items;
 
-                localStorage.setItem('cartNumbers', num);
-                let price = json[id][0].price * json[id][0].incart;
-                price = parseInt(localStorage.getItem('totalCost')) - price;
+                    localStorage.setItem('cartNumbers', num);
+                    let price = json[id][0].price * json[id][0].incart;
+                    price = parseInt(localStorage.getItem('totalCost')) - price;
 
-                localStorage.setItem('totalCost', price);
-                delete json[id];
-                localStorage.setItem('itemsInCart', JSON.stringify(json));
-            })
+                    localStorage.setItem('totalCost', price);
+                    delete json[id];
+                    localStorage.setItem('itemsInCart', JSON.stringify(json));
+                })
         }
         window.location.reload();
     }
 }
+
 
 //Fonction pour faire total item dans panier
 function total() {
@@ -342,7 +327,7 @@ function afficherPanier() {
 
     let productsInCart = document.querySelector('.cart.additem');
     document.querySelector('#total').textContent = `Total : ${localStorage.getItem('totalCost')}€`;
-    console.log(cartITems); // Verification produit présent dans le panier TEST
+    console.log(cartITems);                                                     // Verification produit présent dans le panier TEST
 
     if (cartITems && productsInCart) {
         productsInCart.innerHTML = '';
@@ -384,7 +369,7 @@ function afficherPanier() {
 
     for (let i = 0; i < inputs.length; i++) {
         inputs[i].addEventListener("change", (e) => {
-                //console.log(e) TEST
+            //console.log(e) TEST
             let id = inputs[i].getAttribute('id');
             cartITems[id][0].incart = parseInt(inputs[i].value);
             localStorage.setItem('itemsInCart', JSON.stringify(cartITems));
@@ -450,7 +435,7 @@ function orders(id) {
                                                                 <th scope="row col-sm-3">${items._id}</th>
                                                                 <td>${items.name}</td>
                                                                 <td>${items.price / 100 + '€'}</td>
-                                                            </tr>`
+                                                              </tr>`
             total += items.price;
         })
         document.querySelector('.price').textContent = `Prix total : ${total / 100 + '€'}`;
